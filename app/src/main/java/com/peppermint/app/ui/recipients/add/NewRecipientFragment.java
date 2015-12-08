b0258951870ba4ebe59eab22475fc3f55bba958d
Nuno Luz <nluz@mobaton.com>
2015-12-08 17:20:08 +0000

    new title for add avatar dialog

--- app/src/main/java/com/peppermint/app/ui/recipients/add/NewRecipientFragment.java
+++ app/src/main/java/com/peppermint/app/ui/recipients/add/NewRecipientFragment.java
@@ -387,7 +387,7 @@ public class NewRecipientFragment extends Fragment implements View.OnClickListen
 
         mNewAvatarDialog = new CustomListDialog(mActivity);
         mNewAvatarDialog.setCancelable(true);
-        mNewAvatarDialog.setTitleText(R.string.change_photo);
+        mNewAvatarDialog.setTitleText(R.string.select_photo);
         mNewAvatarDialog.setListOnItemClickListener(this);
         mAvatarAdapter = new NavigationListAdapter(mActivity, mAvatarOptions, app.getFontSemibold());
         mNewAvatarDialog.setListAdapter(mAvatarAdapter);
